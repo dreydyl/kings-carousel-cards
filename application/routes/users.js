325cf2c2c19c76571fd28348683610d8df8ace5e
Devon Dy-Liacco <dreydee33@gmail.com>
2021-05-21 02:24:35 -0700

    Implement Login with MySQL

--- application/routes/users.js
+++ application/routes/users.js
@@ -67,4 +67,35 @@ router.post('/register', (req,res,next) => {
   });
 });
 
+router.post('/login',(req,res,next) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  /**
+   * TODO validation
+   */
+
+  let baseSql = "SELECT username, password FROM users WHERE username=? AND password=?";
+  db.execute(baseSql,[username,password])
+  .then(([results, fields]) => {
+    if(results && results.length == 1){
+      successPrint(`user ${username} is logged in`);
+      res.locals.logged = true;
+      res.render('index');
+    } else {
+      throw new UserError("Invalid username and/or password", "/login", 200);
+    }
+  })
+  .catch((err) => {
+    errorPrint("user login failed");
+    if(err instanceof UserError) {
+      errorPrint(err.getMessage());
+      res.status(err.getStatus());
+      res.redirect('/login');
+    } else {
+      next(err);
+    }
+  })
+});
+
 module.exports = router;
